ffef7f41a2e5303280794ff15a1e21dc08fe764f
doborovicha <aliaksei.doborovich@gmail.com>
2019-12-03 20:29:33 +0300

    - created class SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="firstName", middlename="middleName", lastname="lastName", nickname="nickName", title="titleTest", company="companyTest",
                         address="addressTest", home="homeTest", mobile="mobileTest", work="workTest", fax="faxTest", email="emailTest", email2="emailTest2",
                         email3="emailTest3", homepage="homePage", bday="12", bmonth="June", byear="1995", aday="15", amonth="October", ayear="2010", address2="secondaryAddress",
                         phone2="secondaryHome", notes="notesTest"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
