c250f0521d1924b07db6aa945a263e3b578016ee
doborovicha <aliaksei.doborovich@gmail.com>
2019-12-23 18:57:11 +0300

    - caching and hashing are added to groups/contacts tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,8 +14,8 @@ def test_add_contact(app):
                                email3="emailTest3", homepage="homePage", bday="12", bmonth="June", byear="1995", aday="15", amonth="October", ayear="2010", address2="secondaryAddress",
                                phone2="secondaryHome", notes="notesTest")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     for i in old_contacts:
         old_contacts.append(contact)
         assert old_contacts[i] == new_contacts[i]
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,14 +6,13 @@ def test_modify_contact_firstname(app):
         app.contact.create(Contact(firstname="test_contact"))
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(firstname="New first name"))
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
-
 
 def test_modify_contact_lastname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test_contact"))
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(lastname="New last name"))
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
