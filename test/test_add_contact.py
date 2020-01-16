1606ee0e42420e534124e4bcbed36183c47d4a9f
doborovicha <aliaksei.doborovich@gmail.com>
2020-01-16 18:48:02 +0300

    -task_19 Load od testing data from json is realized

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,8 @@
-import pytest
 from model.contact import Contact
-from fixture.application import Application
-import pytest
-from data.contacts import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(y) for y in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
